d997450e21c625ee7773c5e8293ce6c5a58e0885
ameedasmah1 <ameed.asmah1@gmail.com>
2024-06-02 11:53:23 +0300

    Finished with LanguageSelector

--- resources/js/types/index.ts
+++ resources/js/types/index.ts
@@ -2,7 +2,7 @@ const translationMap: { [key: string]: () => Promise<any> } = {
     "en:home": () => import("@/locales/en/home.json"),
     "ar:home": () => import("@/locales/ar/home.json"),
     "en:welcome": () => import("@/locales/en/welcome.json"),
-    "ar:welcome": () => import("@/locales/en/welcome.json"),
+    "ar:welcome": () => import("@/locales/ar/welcome.json"),
 
     // Add other language-namespace combinations here
 };
