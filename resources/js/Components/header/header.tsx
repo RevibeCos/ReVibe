83e6f0f82d96f3044730e5a1e644c61917c5969b
ameedasmah1 <ameed.asmah1@gmail.com>
2024-06-06 22:45:45 +0300

    Add Header Static

--- resources/js/Components/header/header.tsx
+++ resources/js/Components/header/header.tsx
@@ -1,13 +1,14 @@
 import { useTranslation } from "react-i18next";
 import LanguageSelector from "./partials/LanguageSelector";
 import { ThemeSelector } from "./partials/theme-selector";
-import { usePage } from "@inertiajs/react";
+import { Link, usePage } from "@inertiajs/react";
+import { Button, Input,Icon } from "@/shadcn";
 const Header = () => {
-    const { t, i18n } = useTranslation(['home']);
+    const { t, i18n } = useTranslation(["home"]);
     const isRTL = i18n.dir() === "rtl";
 
-    const {component,props} = usePage();
-    console.log("🚀 ~ Header ~ props:", props)
+    const { component, props } = usePage();
+    console.log("🚀 ~ Header ~ props:", props);
 
     return (
         <header
@@ -18,8 +19,8 @@ const Header = () => {
             <div className="flex flex-col items-center">
                 <div className=" border-b w-full">
                     <div className="max-w-[1536px] mx-auto flex flex-row items-center justify-between py-2 ">
-                        <p className="font-tajawal-light text-sm">
-                            Whatsapp Number:+00972599043747
+                        <p className="font-tajawal-light text-base">
+                            Whatsapp Number : +00972599043747
                         </p>
                         <div className="flex flex-row items-start gap-x-2">
                             <ThemeSelector />
@@ -27,14 +28,36 @@ const Header = () => {
                         </div>
                     </div>
                 </div>
-                <div className="border-b w-full">
-                    <div className="max-w-[1536px] mx-auto flex flex-row items-center justify-between  ">
-                        <div className="flex flex-row basis-5/6 border-r ">
-                            <p>xxx</p>
+            </div>
+            <div className="border-b w-full">
+                <div className="max-w-[1536px] mx-auto flex flex-row items-center justify-between  ">
+                    <div className="flex flex-row items-center  basis-5/6 border-r py-2">
+                        <div className="flex flex-row basis-2/6">
+                            <Link href="/">
+                                <img src="https://media.discordapp.net/attachments/1239894547761467424/1247590968761258084/revibe_1.png?ex=66633822&is=6661e6a2&hm=ae3be938b001ffff891a82c4cc7c723c8dd8d07e36def28244c3cd20e863487a&=&format=webp&quality=lossless"  />
+                            </Link>
                         </div>
-                        <div className="flex flex-row basis-1/6 border-r ">
-                            <p>xxxx</p>
+                        <div className="flex flex-row basis-4/6">
+                            <div className="flex gap-2 basis-1/3 justify-center">
+                                <div className="relative flex-1 md:grow-0">
+                                    <Icon
+                                        name="search"
+                                        className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground"
+                                    />
+                                    <Input
+                                        type="search"
+                                        placeholder="Search..."
+                                        className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[400px]"
+                                    />
+                                </div>
+                                <Button variant="outline">Search</Button>
+                            </div>
                         </div>
+                    </div>
+                    <div className="flex flex-row items-center justify-center gap-x-2 basis-1/6 border-r py-4 h-32 ">
+                        <Icon name="user-round" />
+                        <Icon name="heart" />
+                        <Icon name="shopping-cart" />
                     </div>
                 </div>
             </div>
